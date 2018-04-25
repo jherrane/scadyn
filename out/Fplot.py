89b8ffdb78c4362ef779486fce6604da68131bce
Joonas Herranen <joonas.herranen@helsinki.fi>
2018-04-25 12:41:26 +0300

    Added cool plot

---
+++ out/Fplot.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+data = np.loadtxt('F.out', skiprows=1)
+xi = data[:, 0]
+psi = data[:, 1]
+F = data[:, 2]
+
+psi_unique = np.unique(psi)
+
+fig = plt.figure()
+
+for p in psi_unique: 
+ angle = str(round(np.arccos(p) * 180 / np.pi)).split('.')[0]
+ label = r'$\psi = ' + angle + '^{\circ}$'
+ 
+ plt.plot(xi[psi == p], F[psi == p], '-', label = label)
+ plt.xlabel(r'\cos \xi')
+ plt.ylabel(r'F', fontsize = 16)
+
+ax = fig.add_subplot(111)
+ax.set_xlim((-1, 1))
+plt.legend()
+plt.show()
