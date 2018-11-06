1f29023f7ec08558883739a1a4a94bf8d716e913
Joonas Herranen <joonas.herranen@helsinki.fi>
2018-11-06 11:06:44 +0200

    Added out/Qplot.py.

--- out/Qplot.py
+++ out/Qplot.py
@@ -1,6 +1,27 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+import sys, getopt
+
+Qfile = 'Q'
+inputfile = ''
+pth = ''
+
+argv = sys.argv[1:]
+try:
+   opts, args = getopt.getopt(argv,"hi:p:")
+except getopt.GetoptError:
+   print 'python Fplot.py -h'
+   sys.exit(2)
+for opt, arg in opts:
+   if opt == '-h' or opt == '-help':
+      print 'Fplot.py -i <inputfile>'
+      print 'Fplot.py -i <inputfile>'
+      sys.exit()
+   elif opt in ("-i"):
+      inputfile = arg
+   elif opt in ("-p"):
+      pth = arg+'/'
 
 fig = plt.figure(figsize=(12,8))
 
@@ -9,7 +30,10 @@ plt.rc('font', family='serif')
 font = {'weight' : 'bold', 'size' : 22}
 mpl.rc('font',**font)
 
-data = np.loadtxt('Q', skiprows=1)
+Qfile = pth+Qfile+inputfile
+print Qfile
+data = np.loadtxt(Qfile, skiprows=1)
+
 cos = data[:, 0]
 Q1 = data[:, 1]
 Q2 = data[:, 2]
@@ -25,7 +49,7 @@ plt.plot(cos, Q2, 'g:', label=r'$Q_{e_2}$')
 plt.xlabel(r'$\cos \theta$')
 plt.ylabel(r'$Q_{e_i}$')
 
-ax = fig.add_subplot(111)
+ax = plt.gca()
 ax.set_xlim((-1,1))
 plt.legend()
-plt.show()
+plt.savefig(Qfile+'.png')
